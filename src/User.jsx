9398d3f415844d908a20f2c1c24e57d898f0b1ae
Naijamerit <victor.ejiogu@ust.edu.ng>
2023-03-06 17:02:00 +0100

    2 files modified

--- src/User.jsx
+++ src/User.jsx
@@ -18,28 +18,27 @@ export default function User() {
   return (
     <div>
       {state.map((user) => {
-        // console.log(user);
+        console.log(user);
         return (
-          <div
-            style={{ display: 'flex', marginTop: '10px', border: 'solid' }}
-            key={user.id}
-          >
+          <div className="card" key={user.id}>
             <div>
               <img src={user.avatar_url} />
             </div>
             <div style={{ margin: '20px' }}>
-              <span style={{ fontSize: '25px' }}>{user.login}</span>
+              <b style={{ fontSize: '25px' }}>{user.login}</b>
               <br />
               <span>
-                <a href={user.events_url}>Events</a>
-              </span>
-              <br />
-              <span>
-                <a href={user.followers_url}>Followers</a>
-              </span>
-              <br />
-              <span>
-                <a href={user.repos_url}>Repository</a>
+                Type:
+                <b style={{ color: 'red' }}> {user.type}</b>
+                <br />
+                <a href={user.events_url}> Events </a>|
+                <a href={user.followers_url}> Followers</a> |
+                <a href={user.following_url}> Following</a> |
+                <a href={user.gists_url}> Gist</a> |<br/>
+                <a href={user.url}> Web</a> |
+                <a href={user.repos_url}> Repository</a><br/>
+                <a style={{backgroundColor:'green', border:'solid green', borderRadius:'50px', color:'white', fontSize: '10px'}} href={user.subscriptions_url} >Subscribe</a>
+                <br />
               </span>
             </div>
           </div>
