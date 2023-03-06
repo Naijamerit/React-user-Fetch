5c79f9c616c02c87d7a040644b3f946d1f5405e8
Naijamerit <victor.ejiogu@ust.edu.ng>
2023-03-06 15:16:52 +0100

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
+import React from 'react';
+import UserList from './User';
 
 export default function App() {
-  const [state, setState] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-      .then((res) => res.json())
-      .then((data) => {
-        setState(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
   return (
     <div>
-      {state.map((user) => {
-        console.log(user);
-        return (
-          <div style={{display:'flex'}} key={user.id}>
-            <div>
-              <img src={user.avatar_url} width="100" />
-            </div>
-            <div>
-              <span>{user.login}</span><br/>
-              <span><a href={user.events_url}>Events</a></span><br/>
-              <span><a href={user.followers_url}>Followers</a></span><br/>
-              <span><a href={user.repos_url}>Repository</a></span>
-            </div>
-          </div>
-        );
-      })}
+      <UserList />
     </div>
   );
 }
---
+++ src/User.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+export default function User() {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {state.map((user) => {
+        console.log(user);
+        return (
+          <div
+            style={{ display: 'flex', marginTop: '10px', border: 'solid' }}
+            key={user.id}
+          >
+            <div>
+              <img src={user.avatar_url} />
+            </div>
+            <div style={{ margin: '20px' }}>
+              <span style={{ fontSize: '25px' }}>{user.login}</span>
+              <br />
+              <span>
+                <a href={user.events_url}>Events</a>
+              </span>
+              <br />
+              <span>
+                <a href={user.followers_url}>Followers</a>
+              </span>
+              <br />
+              <span>
+                <a href={user.repos_url}>Repository</a>
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
